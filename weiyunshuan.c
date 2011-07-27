c91c1eaa7f25813c8848c8f2fee30e43450a7339
liuyu <1101456843@163.com>
2011-07-27 10:23:05 +0800

    wei yun shuan

---
+++ weiyunshuan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+    char a=128;
+    int b;
+    if (b==128)
+    printf ("hello!\n");
+    else
+    printf ("wold!\n");
+    return 0;
+}
